092ca45411f5e98bc0b38e6f1beb1ce343d4e949
Thesit <41662696+Thesit@users.noreply.github.com>
2018-12-20 22:03:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'zIz2bsXdEmFUfM5q8zxX/Dcs8UZVujv7JFShA2YFnBVTQv7Hh5vAS2dPyQcyq/AhRF0MO9RA8lZH+cNFM3+xrUx9rmJWzt9bPiLhBrWOjnGknHr82Whg0qOtQGcBBj5tA0MhtPhNB8I2f5mRJbuKpwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
